2c98e94077851bc0e71f089d17d7a03abcc53df0
RicardoReis <us.rreis@formacao.ips.pt>
2023-12-19 11:18:55 +0000

    27

---
+++ resources/JavaDoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"adapters"},{"l":"All Packages","u":"allpackages-index.html"},{"l":"artsy"},{"l":"domain"},{"l":"persistence"},{"l":"presenter"},{"l":"server"},{"l":"services"},{"l":"util"},{"l":"view"}];updateSearchResults();
