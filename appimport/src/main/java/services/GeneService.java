8c17da5800e72f744ad457ace8d40d0a3a4bf4b2
us-mspinola <us.mspinola@formacao.ips.pt>
2023-12-19 00:40:35 +0000

    GeneService javadoc

--- appimport/src/main/java/services/GeneService.java
+++ appimport/src/main/java/services/GeneService.java
@@ -13,6 +13,10 @@ import java.time.OffsetDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Service class for handling operations related to Gene entities.
+ * Implements the IService interface for Gene objects.
+ */
 public class GeneService implements IService<Gene> {
 
     private static final OkHttpClient httpClient = new OkHttpClient();
@@ -20,6 +24,12 @@ public class GeneService implements IService<Gene> {
             .registerTypeAdapter(OffsetDateTime.class, new LocalDateAdapter())
             .create();
 
+    /**
+     * Creates a new Gene in the persistent storage.
+     *
+     * @param apiUrl The URL of the API where the gene is to be created.
+     * @param gene The gene object to be created.
+     */
     public void create(String apiUrl, Gene gene) {
         String json = gson.toJson(gene);
         RequestBody body = RequestBody.create(MediaType.parse("application/json"), json);
@@ -37,6 +47,13 @@ public class GeneService implements IService<Gene> {
         }
     }
 
+
+    /**
+     * Retrieves all genes from the persistent storage.
+     *
+     * @param apiUrl The URL of the API from where genes are to be fetched.
+     * @return A list of Gene objects.
+     */
     public List<Gene> getAll(String apiUrl) {
         List<Gene> all = new ArrayList<>();
         Request getRequest = new Request.Builder().url(apiUrl).build();
@@ -55,6 +72,12 @@ public class GeneService implements IService<Gene> {
         return all;
     }
 
+    /**
+     * Logs the error response from the HTTP request.
+     *
+     * @param response The HTTP response to log.
+     * @throws IOException If an I/O error occurs.
+     */
     private void logErrorResponse(Response response) throws IOException {
         // Replace with a proper logging mechanism
         System.out.println("Error: " + response.code() + " - " + response.message());
