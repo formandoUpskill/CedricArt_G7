9f21507864831abadb27f1189f5e02c8372703da
us-mspinola <us.mspinola@formacao.ips.pt>
2023-12-19 00:42:38 +0000

    PartnerService javadoc

--- appimport/src/main/java/services/PartnerService.java
+++ appimport/src/main/java/services/PartnerService.java
@@ -13,6 +13,10 @@ import java.time.OffsetDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Service class for handling operations related to Partner entities.
+ * Implements the IService interface for Partner objects.
+ */
 public class PartnerService implements IService<Partner> {
 
     private static final OkHttpClient httpClient = new OkHttpClient();
@@ -20,6 +24,12 @@ public class PartnerService implements IService<Partner> {
             .registerTypeAdapter(OffsetDateTime.class, new LocalDateAdapter())
             .create();
 
+    /**
+     * Creates a new Partner in the persistent storage.
+     *
+     * @param apiUrl The URL of the API where the partner is to be created.
+     * @param partner The partner object to be created.
+     */
     public void create(String apiUrl, Partner partner) {
         String json = gson.toJson(partner);
         RequestBody body = RequestBody.create(MediaType.parse("application/json"), json);
@@ -37,6 +47,13 @@ public class PartnerService implements IService<Partner> {
         }
     }
 
+
+    /**
+     * Retrieves all partners from the persistent storage.
+     *
+     * @param apiUrl The URL of the API from where partners are to be fetched.
+     * @return A list of Partner objects.
+     */
     public List<Partner> getAll(String apiUrl) {
         List<Partner> all = new ArrayList<>();
         Request getRequest = new Request.Builder().url(apiUrl).build();
@@ -55,6 +72,13 @@ public class PartnerService implements IService<Partner> {
         return all;
     }
 
+
+    /**
+     * Logs the error response from the HTTP request.
+     *
+     * @param response The HTTP response to log.
+     * @throws IOException If an I/O error occurs.
+     */
     private void logErrorResponse(Response response) throws IOException {
         // Replace with a proper logging mechanism
         System.out.println("Error: " + response.code() + " - " + response.message());
