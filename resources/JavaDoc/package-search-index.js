025ccdd451154bcd209cfc62b693d508b9afea45
RicardoReis <us.rreis@formacao.ips.pt>
2023-12-20 14:48:17 +0000

    30

--- resources/JavaDoc/package-search-index.js
+++ resources/JavaDoc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"adapters"},{"l":"All Packages","u":"allpackages-index.html"},{"l":"artsy"},{"l":"domain"},{"l":"persistence"},{"l":"presenter"},{"l":"server"},{"l":"services"},{"l":"util"},{"l":"view"}];updateSearchResults();
+packageSearchIndex = [{"l":"adapters"},{"l":"All Packages","u":"allpackages-index.html"},{"l":"artsy"},{"l":"domain"},{"l":"persistence"},{"l":"presenter"},{"l":"run"},{"l":"server"},{"l":"services"},{"l":"util"},{"l":"view"}];updateSearchResults();
